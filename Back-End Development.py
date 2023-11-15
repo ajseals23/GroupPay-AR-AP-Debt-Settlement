a855b275b8867462cb7763b7062e0dd926052e74
ajseals23 <128306760+ajseals23@users.noreply.github.com>
2023-11-15 18:16:45 -0500

    Add classes to Back-End Development.py

--- Back-End Development.py
+++ Back-End Development.py
@@ -1,7 +1,22 @@
-#!/usr/bin/env python
-# coding: utf-8
+class User:
+    def __init__(self, user_id, name):
+        self.user_id = user_id
+        self.name = name
+        self.groups = []
 
-# In[ ]:
+class Group:
+    def __init__(self, group_id, name):
+        self.group_id = group_id
+        self.name = name
+        self.members = []
+        self.invoices = []
+
+class Invoice:
+    def __init__(self, invoice_id, amount, issuer_id, receiver_id):
+        self.invoice_id = invoice_id
+        self.amount = amount
+        self.issuer_id = issuer_id
+        self.receiver_id = receiver_id
 
 
 
