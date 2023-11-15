8a31af9afa23b59cb7ccb135374a58792c5ff410
ajseals23 <128306760+ajseals23@users.noreply.github.com>
2023-11-15 18:29:01 -0500

    Read our dataset

--- Back-End Development.py
+++ Back-End Development.py
@@ -1,23 +1,15 @@
-class User:
-    def __init__(self, user_id, name):
-        self.user_id = user_id
-        self.name = name
-        self.groups = []
+import pandas as pd
 
-class Group:
-    def __init__(self, group_id, name):
-        self.group_id = group_id
-        self.name = name
-        self.members = []
-        self.invoices = []
+# Read Construction_Contract data
 
-class Invoice:
-    def __init__(self, invoice_id, amount, issuer_id, receiver_id):
-        self.invoice_id = invoice_id
-        self.amount = amount
-        self.issuer_id = issuer_id
-        self.receiver_id = receiver_id
+csv_filepath = "~/Desktop/Construction_Contracts.csv"
+df_csv = pd.read_csv(csv_filepath)
 
+
+
+
+
+    
 
 
 
