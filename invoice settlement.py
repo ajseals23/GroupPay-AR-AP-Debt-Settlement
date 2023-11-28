42e53183bb306a69f82d9e9661a0c0573873451e
strangenicholas <nicholaswstrange@gmail.com>
2023-11-28 08:44:07 -0500

    add notes

--- invoice settlement.py
+++ invoice settlement.py
@@ -11,5 +11,7 @@ vendor_AP = df.groupby('contractor_id')['revised_amount'].sum().reset_index()
 # Calculate Accounts Recievable
 vendor_AR = df.groupby('vendor_id')['revised_amount'].sum().reset_index()
 
-# need to add 
+# need to calculate net amount based on total AP & AR
+# need to categorize by aging
+# need to create meaure to show how much need
 
